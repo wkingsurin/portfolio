be111ce166a85a78cfbc7987a145caa144a6e9a3
wkingsurin <wkingsurin@gmail.com>
2024-01-14 22:02:34 +0500

    Created components for 'stack' tab

--- src/Components/MyMain/TabContent/Card.jsx
+++ src/Components/MyMain/TabContent/Card.jsx
@@ -1,13 +1,11 @@
+import { Children } from "react";
 import classes from "./TabContent.module.scss";
 
-export default function Card({ children, src, alt, svg }) {
+export default function Card({ name, src, alt, children }) {
   return (
     <div className={classes.card}>
-      <div className={classes["card-image-block"]}>
-        {svg}
-        {/* <img src={src} alt={alt} /> */}
-      </div>
-      <span className={classes["card-text"]}>{children}</span>
+      <div className={classes["card-image-block"]}>{children}</div>
+      <span className={classes["card-text"]}>{name}</span>
     </div>
   );
 }
