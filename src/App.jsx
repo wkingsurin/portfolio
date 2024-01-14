5dae591339b799f110d77e4ada6725cdb1e326fa
wkingsurin <wkingsurin@gmail.com>
2024-01-15 01:37:55 +0500

    Fixed a bug with jumping scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,33 @@ import "normalize.css";
 import Header from "./Components/Header/Header";
 import MyMain from "./Components/MyMain/MyMain";
 
+function setScrollWidth() {
+  const outer = document.createElement("div");
+
+  outer.style.visibility = "hidden";
+  outer.style.overflow = "scroll";
+
+  document.body.append(outer);
+
+  const inner = document.createElement("div");
+  outer.append(inner);
+
+  const scrollWidth = outer.offsetWidth - inner.offsetWidth;
+
+  document.documentElement.style.setProperty(
+    "--scroll-width",
+    `${scrollWidth}`
+  );
+
+  outer.remove();
+}
+
 export default function App() {
   const [tabName, setTabName] = useState("works");
+  useEffect(() => {
+    setScrollWidth();
+    document.body.style.width = window.innerWidth + "px";
+  }, []);
 
   const onClick = (e) => {
     setTabName(() => e.target.getAttribute("name"));
